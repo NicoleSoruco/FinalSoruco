930a45f4a54939388b25f5699d1c46230f9b13b4
nicole soruco cuellar <nicolita.01@hotmail.com>
2020-12-12 11:55:07 -0400

    finalizado

---
+++ SorucoFin/SorucoFin/Controllers/SorucoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SorucoFin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SorucoController : ControllerBase
+    {
+        [HttpGet]
+        public string Operaciones (int numero)
+        {
+            if (numero > 0)
+                return "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+            else if (numero == 0)
+                return "Realizado por Nicole Soruco";
+            else return "ERROR";
+        }
+    }
+}
